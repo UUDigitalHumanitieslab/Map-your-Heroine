9d4d262d6a12680df156aea4a3e2b2b05e689524
lukavdplas <l.p.vanderplas@uu.nl>
2021-10-01 12:00:12 +0200

    remove bokeh from environment

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// import * as Widgets from '@bokeh/bokehjs/build/js/lib/models/widgets/index';
-// import * as Tables from '@bokeh/bokehjs/build/js/lib/models/widgets/tables/index';
-// import { register_models } from '@bokeh/bokehjs/build/js/lib/base';
-
 
 if (environment.production) {
   enableProdMode();
