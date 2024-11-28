f29689a5cca2ce589fceae57aa7078eb41a9c7b4
Jelte van Boheemen <j.vanboheemen@uu.nl>
2024-11-28 12:02:58 +0100

    Add X-axis label to Likert plots

--- frontend/src/app/models/plotdata.ts
+++ frontend/src/app/models/plotdata.ts
@@ -1,16 +1,24 @@
 export const LIKERTPLOTOPTIONS = {
-    aspectRatio: 4,
-    responsive: true,
-    scales: {
-        xAxes: [{
-            display: true,
-        }],
-        yAxes: [{
-            display: false,
-            ticks: { min: 0 }
-        }]
-    },
-    legend: {
-        display: false
-    }
+  aspectRatio: 4,
+  responsive: true,
+  scales: {
+    xAxes: [
+      {
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: "1 = Strongly Disagree, 5 = Strongly Agree",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        display: false,
+        ticks: { min: 0 },
+      },
+    ],
+  },
+  legend: {
+    display: false,
+  },
 };
